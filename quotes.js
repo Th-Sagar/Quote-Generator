03027d9d71f9d3ddc8847a048063552089f1a31a
Th-Sagar <sagarthapa98122@gmail.com>
2024-09-07 20:34:44 +0545

    feat: next, random, previous

---
+++ quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+  {
+    id: 1,
+    title:
+      "Science is a way of thinking much more than it is a body of knowledge.",
+    category: "science",
+  },
+  {
+    id: 2,
+    title: "The important thing is to never stop questioning.",
+    category: "science",
+  },
+  {
+    id: 3,
+    title:
+      "Success is not the key to happiness. Happiness is the key to success.",
+    category: "success",
+  },
+  {
+    id: 4,
+    title: "Don't watch the clock; do what it does. Keep going.",
+    category: "success",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import quotes from "./quotes.js";
+
 const plus = document.getElementById("plus");
 const minu = document.getElementById("minus");
 const previous = document.getElementById("previous");
@@ -5,7 +7,9 @@ const randomQuote = document.getElementById("random");
 const next = document.getElementById("next");
 const title = document.getElementById("title");
 const theme = document.getElementById("theme-toggle");
-
+const categoryOptions = document.getElementById("categoryOptions");
+let currentIndex = 0;
+let filteredQuotes = quotes;
 //theme
 theme.addEventListener("click", () => {
   const body = document.body;
@@ -36,3 +40,55 @@ minu.addEventListener("click", () => {
     .getPropertyValue("font-size");
   title.style.fontSize = parseInt(fontsize) - 1 + "px";
 });
+
+//display quote
+
+function show(display) {
+  if (filteredQuotes.length > 0)
+    title.textContent = filteredQuotes[display].title;
+  else title.textContent = " Quotes";
+}
+
+//change category
+
+categoryOptions.addEventListener("change", () => {
+  currentIndex = 0;
+  const selectedCategory = categoryOptions.value;
+
+  if (selectedCategory === "all") {
+    filteredQuotes = quotes;
+  } else {
+    filteredQuotes = quotes.filter(
+      (quote) => quote.category === selectedCategory
+    );
+  }
+
+  show(currentIndex);
+});
+
+//next
+
+next.addEventListener("click", () => {
+  if (currentIndex < filteredQuotes.length - 1) {
+    currentIndex++;
+    show(currentIndex);
+  }
+});
+
+//previous
+
+previous.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    show(currentIndex);
+  }
+});
+
+//random
+
+randomQuote.addEventListener("click", () => {
+  currentIndex = Math.floor(Math.random() * filteredQuotes.length);
+  show(currentIndex);
+});
+
+show(currentIndex);
